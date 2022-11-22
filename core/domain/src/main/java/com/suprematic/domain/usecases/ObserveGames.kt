c67a0ad0783c298806452e324069e3f5a6024052
sakalaus@gmai.com <sakalaus@gmail.com>
2022-11-22 14:43:28 +0200

    Date formatted hoisted from Ui to use cases Gitignore

--- core/domain/src/main/java/com/suprematic/domain/usecases/ObserveGames.kt
+++ core/domain/src/main/java/com/suprematic/domain/usecases/ObserveGames.kt
@@ -2,13 +2,25 @@ package com.suprematic.domain.usecases
 
 import com.suprematic.domain.Repository
 import com.suprematic.domain.entities.Game
-import com.suprematic.domain.entities.Sport
-import com.suprematic.domain.entities.Team
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
+
+const val dateFormatPattern = "dd.MM.yyyy hh:mm"
 
 class ObserveGames @Inject constructor(
     private val repository: Repository
 ) {
-    operator fun invoke(): Flow<List<Game>> = repository.observeGames()
+    private val standardDateFormat = SimpleDateFormat(dateFormatPattern, Locale.getDefault())
+    operator fun invoke(): Flow<Map<Game, String>> =
+        repository.observeGames().map { games ->
+            games.associateWith { game ->
+                standardDateFormat.format(Calendar.getInstance().apply {
+                    timeInMillis = game.timeStamp
+                }
+                    .time)
+            }
+        }
 }
